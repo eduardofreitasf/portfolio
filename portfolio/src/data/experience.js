365eec9c8a35f26db55416a9b35deb7cce677f1b
eduardofreitasf <eduardof.fernandes05@gmail.com>
2026-05-29 22:42:49 +0100

    chore: add member in EPIC to experience

--- portfolio/src/data/experience.js
+++ portfolio/src/data/experience.js
@@ -1,12 +1,22 @@
 export const experiences = [
   {
     id: 1,
+    role: "Member - Projects Department",
+    organization: "EPIC Junior",
+    orgUrl: "https://epicje.pt/",
+    period: "05/2026 - present",
+    description:
+      "Member of EPIC Junior, the junior enterprise of the School of Engineering at Universidade do Minho. Collaborating as a member in the Projects Department on real-world software development projects.",
+    current: true,
+  },
+  {
+    id: 2,
     role: "Trainee - Projects Department",
     organization: "EPIC Junior",
     orgUrl: "https://epicje.pt/",
-    period: "04/2026 - present",
+    period: "04/2026 - 05/2026",
     description:
-      "Member of EPIC Junior, the junior enterprise of the School of Engineering at Universidade do Minho. Collaborating as a trainee in the Projects Department on real-world software development projects.",
-    current: true,
+      "Trainee at EPIC Junior, the junior enterprise of the School of Engineering at Universidade do Minho.",
+    current: false,
   },
 ];
